1d0637d12e539ecd838ed2614b2ab329df502e51
Fionn Murphy <a@b.c>
2018-06-15 11:34:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var appliances; 
+appliances=['fridge','microwave','freezer','television'];
+var daysofweek; 
+daysofweek=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+var daysineachmonth; 
+daysineachmonth=[31,28,31,30,31,30,31,31,30,31,30,31];
+var stringNumberBoolean; 
+stringNumberBoolean=['I am a dog','And this is my Mom',11,13,17,true];
+var metals; 
+metals=['gold','silver','platinum'];
+
+var el = document.getElementById('appliances');
+var el2 = document.getElementById('daysofweek');
+var el3 = document.getElementById('daysineachmonth');
+var el4 = document.getElementById('string number boolean');
+var el5 = document.getElementById('metals');
+
+el.textContent = appliances[2];
+el2.textContent = daysofweek[6];
+//el3.textContent = daysineachmonth[]
+
